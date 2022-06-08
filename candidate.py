d84bd5fe3a070fd419ea5e0cdd1bb72dfbe384fe
sajabilal <102521358+sajabilal@users.noreply.github.com>
2022-06-08 16:35:18 +0300

    Update candidate.py

--- candidate.py
+++ candidate.py
@@ -2,8 +2,8 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/saja')
-def saja():
+@app.route('/candidate')
+def candidate():
     return 'welcome to the candidate webpage  :) :)  '
 
 app.run(host='0.0.0.0', port=5000)
